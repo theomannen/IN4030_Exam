b31b30b48e3887cf25ddb8b9c27bd97d2cc76d44
Theodor Bergersen <mtberger@uio.no>
2022-03-25 12:33:45 +0100

    Done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 import sys
 
 def readFASTA(filename):
@@ -54,33 +55,37 @@ def makeMatrix(blosum, v, w, penalty, penalty_ext):
     return scoreMatrix, backtrack
 
 def OutputLCS(backtrack, v, w, i, j, v_, w_):
-    if i == 0 or j == 0 or backtrack[i-1][j-1] == 3:
+    if i == 0 or j == 0:
+        return v[i]+v_, w[j]+w_
+    elif backtrack[i][j] == 3:
         return v_, w_
-    if backtrack[i][j] == 0:
-        return OutputLCS(backtrack, v, w, i - 1, j, v[i-1]+v_, '-'+w_)
+    elif backtrack[i][j] == 0:
+        return OutputLCS(backtrack, v, w, i - 1, j, v[i]+v_, '-'+w_)
     elif backtrack[i][j] == 1:
-        return OutputLCS(backtrack, v, w, i, j - 1, '-'+v_, w[j-1]+w_)
+        return OutputLCS(backtrack, v, w, i, j - 1, '-'+v_, w[j]+w_)
     else:
-        return OutputLCS(backtrack, v, w, i - 1, j - 1, v[i-1]+v_, w[j-1]+w_)
+        return OutputLCS(backtrack, v, w, i - 1, j - 1, v[i]+v_, w[j]+w_)
 
 if __name__ == "__main__":
-    penalty_ext = 1
-    penalty = 11
-    try:
-        penalty = int(sys.argv[1])
-        penalty_ext = float(sys.argv[2])
-    except IndexError:
-        print('Use case of the program is \"python3 main.py penalty penalty_ext\"')
-    v = readFASTA('human')
-    w = readFASTA('yeast')
+    parser = argparse.ArgumentParser(description='Compare two protein sequences and find alignment')
+    parser.add_argument('-p', metavar='--penalty', type=int, default=12, help='The initial penalty of gap opening')
+    parser.add_argument('-pe', metavar='--penalty_ext', type=int, default=1, help='The penalty of gap extension')
+    parser.add_argument('-f1', metavar='--file1', required=True, help='The name of the Query sequence file --REQUIRED')
+    parser.add_argument('-f2', metavar='--file2', required=True, help='The name of the Subject sequence file --REQUIRED')
 
-    #v = 'ATTCGTA'
-    #w = 'ATGCTA'
+    p = parser.parse_args()
+    penalty = p.p
+    penalty_ext = p.pe
+    file1 = p.f1
+    file2 = p.f2
+    v = readFASTA(file1)
+    w = readFASTA(file2)
+
     blosum = readBLOSUM('BLOSUM62.txt')
     scoreMatrix, backtrack = makeMatrix(blosum, v, w, penalty, 1)
     score = np.amax(scoreMatrix)
     max_indexes = np.unravel_index(np.argmax(scoreMatrix), scoreMatrix.shape)
-    v_, w_ = OutputLCS(backtrack, v, w, max_indexes[0] + 1, max_indexes[1] + 1, '', '')
+    v_, w_ = OutputLCS(backtrack, v, w, max_indexes[0], max_indexes[1], '', '')
     width = 80
     loops = int(len(v_)/width)
     print(score)
