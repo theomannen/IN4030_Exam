5f500196b1f8f296acd4b47d2b30049072cf6381
mtberger <mtberger@uio.no>
2022-03-25 08:40:07 +0100

    Added Report.txt

--- main.py
+++ main.py
@@ -24,25 +24,21 @@ def readBLOSUM(filename):
     return outerDict
 
 def makeMatrix(blosum, v, w, penalty, penalty_ext):
-    del_matrix = np.zeros(shape=(len(v), len(w)))
-    ins_matrix = np.zeros(shape=(len(v), len(w)))
+    qryMatrix = np.zeros(shape=(len(v), len(w)))
+    sbjMatrix = np.zeros(shape=(len(v), len(w)))
     scoreMatrix = np.zeros(shape=(len(v)+1, len(w)+1))
-    backtrack = np.zeros(shape=(len(v)+1, len(w)+1))
-    o = penalty
-    q = penalty
-    for i in range(1, len(v)+1):
-        for j in range(1, len(w)+1):
-            open_ins = scoreMatrix[i-1][j] - penalty
-            ext_ins = ins_matrix[[i-1][j]] - penalty_ext
-            ins_matrix[i][j] = max(0, open_ins, ext_ins)
+    backtrack = np.zeros(shape=(len(v), len(w)))
 
-            open_del = scoreMatrix[i - 1][j] - penalty
-            ext_del = del_matrix[[i - 1][j]] - penalty_ext
-            del_matrix[i][j] = max(0, open_del, ext_del)
+    for i in range(1, len(v)):
+        for j in range(1, len(w)):
 
-            diagonal = scoreMatrix[i - 1][j - 1] + blosum[w[j-1]][v[i-1]]
-            delete = open_del[i-1][j] - blosum[w[j-1]][v[i-1]]
-            insert = scoreMatrix[i][j-1] - blosum[w[j-1]][v[i-1]]
+            qryMatrix[i][j] = max(0, scoreMatrix[i-1][j] - penalty, qryMatrix[i-1][j] - penalty_ext)
+
+            sbjMatrix[i][j] = max(0, scoreMatrix[i][j-1] - penalty, sbjMatrix[i][j-1] - penalty_ext)
+
+            diagonal = scoreMatrix[i - 1][j - 1] + blosum[v[i]][w[j]]
+            delete = qryMatrix[i - 1][j - 1] + blosum[v[i]][w[j]]
+            insert = sbjMatrix[i - 1][j - 1] + blosum[v[i]][w[j]]
 
             scoreMatrix[i][j] = max(diagonal, 0, insert, delete)
 
@@ -54,27 +50,27 @@ def makeMatrix(blosum, v, w, penalty, penalty_ext):
                 backtrack[i][j] = 0
             elif scoreMatrix[i][j] == 0:
                 backtrack[i][j] = 3
-    print(scoreMatrix)
-    print(backtrack)
+
     return scoreMatrix, backtrack
 
 def OutputLCS(backtrack, v, w, i, j, v_, w_):
-  if i == 0 or j == 0 or backtrack[i][j] == 3:
-    return v_, w_
-  if backtrack[i][j] == 0:
-    return OutputLCS(backtrack, v, w, i - 1, j, v[i-1]+v_, '-'+w_)
-  elif backtrack[i][j] == 1:
-    return OutputLCS(backtrack, v, w, i, j - 1, '-'+v_, w[j-1]+w_)
-  else:
-    return OutputLCS(backtrack, v, w, i - 1, j - 1, v[i-1]+v_, w[j-1]+w_)
+    if i == 0 or j == 0 or backtrack[i-1][j-1] == 3:
+        return v_, w_
+    if backtrack[i][j] == 0:
+        return OutputLCS(backtrack, v, w, i - 1, j, v[i-1]+v_, '-'+w_)
+    elif backtrack[i][j] == 1:
+        return OutputLCS(backtrack, v, w, i, j - 1, '-'+v_, w[j-1]+w_)
+    else:
+        return OutputLCS(backtrack, v, w, i - 1, j - 1, v[i-1]+v_, w[j-1]+w_)
 
 if __name__ == "__main__":
+    penalty_ext = 1
     penalty = 11
-    if sys.argv[1]:
-        try:
-            penalty = int(sys.argv[1])
-        except ValueError:
-            print('Use case of the program is \"python3 main.py INTEGER\"')
+    try:
+        penalty = int(sys.argv[1])
+        penalty_ext = float(sys.argv[2])
+    except IndexError:
+        print('Use case of the program is \"python3 main.py penalty penalty_ext\"')
     v = readFASTA('human')
     w = readFASTA('yeast')
 
@@ -84,7 +80,7 @@ if __name__ == "__main__":
     scoreMatrix, backtrack = makeMatrix(blosum, v, w, penalty, 1)
     score = np.amax(scoreMatrix)
     max_indexes = np.unravel_index(np.argmax(scoreMatrix), scoreMatrix.shape)
-    v_, w_ = OutputLCS(backtrack, v, w, max_indexes[0], max_indexes[1], '', '')
+    v_, w_ = OutputLCS(backtrack, v, w, max_indexes[0] + 1, max_indexes[1] + 1, '', '')
     width = 80
     loops = int(len(v_)/width)
     print(score)
